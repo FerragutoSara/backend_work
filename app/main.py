b7326518b966f6947e9d5cc1ec3a6b8d7d1f2fa2
fl-fabio <fl.fabio.lanzafame@gmail.com>
2026-04-09 12:41:40 +0200

    feature: ci/cd configuration

--- app/main.py
+++ app/main.py
@@ -3,7 +3,8 @@ from app.routers import router as api_router
 
 app = FastAPI(
     title="FastAPI MongoDB Atlas CRUD",
-    version="1.0.0"
+    version="1.0.0",
+    root_path="/api"
 )
 
 app.include_router(api_router)
