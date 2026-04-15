137e0e577736a52dac9a59860b6a21b474820b9f
Sara Ferraguto <ferragutosara03@gmail.com>
2026-04-15 11:56:07 +0200

    modify Mongo collection connection

--- app/core/config.py
+++ app/core/config.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 MONGO_URI = os.getenv("MONGO_URI")
-MONGO_DB = os.getenv("MONGO_DB", "users-crud")
+MONGO_DB = os.getenv("MONGO_DB", "skillbridge-db")
 MONGO_COLLECTION_USERS = os.getenv("MONGO_COLLECTION_USERS", "users")
 MONGO_COLLECTION_AUTH = os.getenv("MONGO_COLLECTION_AUTH", "auth")
 JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "secret-token-key")
