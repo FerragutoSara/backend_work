daea1ed71b6a8e4c4332cf355c98f1ce3f6b8233
fl-fabio <fl.fabio.lanzafame@gmail.com>
2026-04-15 09:19:23 +0200

    feature: allow * origins

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import router as api_router
 
 app = FastAPI(
     title="FastAPI MongoDB Atlas CRUD",
     version="1.0.0",
     root_path="/api"
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(api_router)
